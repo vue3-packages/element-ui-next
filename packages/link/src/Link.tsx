4cd8e5c99adaf043d3e8aeb8e46ba2f0ccc4addb
张黎67 <zhangli67@hikvision.com>
2020-07-23 17:20:30 +0800

    feat: link

--- packages/link/src/Link.tsx
+++ packages/link/src/Link.tsx
@@ -1,39 +1,52 @@
 import {defineComponent, PropType} from "vue"
+// import { useEventBus } from "../../menu/src/menuHooks";
 // import { ElFormSymbol, ElFormItemSymbol, useGlobal } from "../../provides/index"
+import eventBus from "../../../src/tools/eventBus";
 
 const ElLink = defineComponent({
   name: "ElLink",
   props: {
     type: {
-        type: String as PropType<
-        "primary" | "success" | "warning" | "danger" | "info"
-        >,
-        default: "default"
-      },
-    icon: { type: String, default: "" },
-    underline: { type: Boolean },
-    disabled: { type: Boolean }
+      type: String,
+      default: "default"
+    },
+    underline: {
+      type: Boolean,
+      default: true
+    },
+    disabled: Boolean,
+    href: String,
+    icon: String
   },
   setup (props,  { attrs, slots }) {
-    // const elForm = inject(ElFormSymbol, null)
-    // const linkUnderline = computed(() => {
-    //     return props.underline || elForm?.underline
-    //   })
+    // const eventBus = useEventBus()
+    const handleClick = (event) => {
+      if (!props.disabled) {
+        if (!props.href) {
+          eventBus.emit("click", event);
+        }
+      }
+    }
     return () => (
-      <a
-        class={[
-          "el-link",
-          `el-link--${props.type}`,
-          {
-            "is-underline": props.underline,
-            "is-disabled": props.disabled,
-          }
-      ]}
-      {...attrs}
-      > 
-      {props.icon && <i class={props.icon}></i>}
-        <span class='el-link--inner'></span>
-      </a>
+      <a class={[
+            "el-link",
+            props.type ? `el-link--${props.type}` : "",
+            props.disabled && "is-disabled",
+            props.underline && !props.disabled && "is-underline"
+          ]}
+            href={props.disabled ? "" : props.href}
+            {...attrs}
+            onClick={handleClick}
+      >
+
+    <i :class="icon" v-if="icon"></i>
+
+    <span v-if="$slots.default" class="el-link--inner">
+      <slot></slot>
+    </span>
+
+    <template v-if="$slots.icon"><slot v-if="$slots.icon" name="icon"></slot></template>
+  </a>
     )
   }
 })
