2a7053b9b51c55308131f3e03b2b12cc00cedd1f
zhangli <zhangli@singularityclub.net>
2020-07-27 21:56:30 +0800

    feat: rate(75%)

--- src/mixins/migrating.ts
+++ src/mixins/migrating.ts
@@ -1,54 +1,39 @@
 import { kebabCase } from "../../src/utils/util";
-/**
- * Show migrating guide in browser console.
- *
- * Usage:
- * import Migrating from 'element-ui/src/mixins/migrating';
- *
- * mixins: [Migrating]
- *
- * add getMigratingConfig method for your component.
- *  getMigratingConfig() {
- *    return {
- *      props: {
- *        'allow-no-selection': 'allow-no-selection is removed.',
- *        'selection-mode': 'selection-mode is removed.'
- *      },
- *      events: {
- *        selectionchange: 'selectionchange is renamed to selection-change.'
- *      }
- *    };
- *  },
- */
-export default {
+import { onMounted, getCurrentInstance, onUnmounted, Ref} from "vue";
+
+function migrating(): {
+} {
   onMounted(() => {
     if (process.env.NODE_ENV === "production") return;
-    if (!this.$vnode) return;
-    const { props = {}, events = {} } = this.getMigratingConfig();
-    const { data, componentOptions } = this.$vnode;
+    const componentInternalInstance = getCurrentInstance()
+    if (componentInternalInstance && !componentInternalInstance.vnode) return;
+    const { props = {}, events = {} } = getMigratingConfig();
+    const { data, componentOptions } = componentInternalInstance.vnode;
     const definedProps = data.attrs || {};
     const definedEvents = componentOptions.listeners || {};
 
     for (let propName in definedProps) {
       propName = kebabCase(propName); // compatible with camel case
       if (props[propName]) {
-        console.warn(`[Element Migrating][${this.$options.name}][Attribute]: ${props[propName]}`);
+        console.warn(`[Element Migrating][${componentInternalInstance.options.name}][Attribute]: ${props[propName]}`);
       }
     }
 
     for (let eventName in definedEvents) {
       eventName = kebabCase(eventName); // compatible with camel case
       if (events[eventName]) {
-        console.warn(`[Element Migrating][${this.$options.name}][Event]: ${events[eventName]}`);
+        console.warn(`[Element Migrating][${componentInternalInstance.options.name}][Event]: ${events[eventName]}`);
       }
     }
-  }),
-  methods: {
-    const getMigratingConfig = () => {
-      return {
-        props: {},
-        events: {},
-      };
-    },
-  },
-};
+  });
+  const getMigratingConfig = () => {
+    return {
+      props: {},
+      events: {},
+    };
+  }
+  return {
+  };
+}
+
+export default migrating;
