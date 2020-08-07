8ab7f5b12ce0b7acfe6992cc0b71163a3a14e6c1
xiaoeyuZZZ <772219457@qq.com>
2020-08-07 08:35:29 +0800

    feat: step 90%

--- packages/steps/src/step.tsx
+++ packages/steps/src/step.tsx
@@ -1,190 +1,4 @@
-// <template>
-//   <div
-//     class="el-step"
-//     :style="style"
-//     :class="[
-//       !isSimple && `is-${$parent.direction}`,
-//       isSimple && 'is-simple',
-//       isLast && !space && !isCenter && 'is-flex',
-//       isCenter && !isVertical && !isSimple && 'is-center'
-//      ]">
-//     <!-- icon & line -->
-//     <div
-//       class="el-step__head"
-//       :class="`is-${currentStatus}`">
-//       <div
-//         class="el-step__line"
-//         :style="isLast ? '' : { marginRight: $parent.stepOffset + 'px' }"
-//       >
-//         <i class="el-step__line-inner" :style="lineStyle"></i>
-//       </div>
-//
-//       <div class="el-step__icon" :class="`is-${icon ? 'icon' : 'text'}`">
-//         <slot
-//           v-if="currentStatus !== 'success' && currentStatus !== 'error'"
-//           name="icon">
-//           <i v-if="icon" class="el-step__icon-inner" :class="[icon]"></i>
-//           <div class="el-step__icon-inner" v-if="!icon && !isSimple">{{ index + 1 }}</div>
-//         </slot>
-//         <i
-//           v-else
-//           :class="['el-icon-' + (currentStatus === 'success' ? 'check' : 'close')]"
-//           class="el-step__icon-inner is-status"
-//         >
-//         </i>
-//       </div>
-//     </div>
-//     <!-- title & description -->
-//     <div class="el-step__main">
-//       <div
-//         class="el-step__title"
-//         ref="title"
-//         :class="['is-' + currentStatus]">
-//         <slot name="title">{{ title }}</slot>
-//       </div>
-//       <div v-if="isSimple" class="el-step__arrow"></div>
-//       <div
-//         v-else
-//         class="el-step__description"
-//         :class="['is-' + currentStatus]">
-//         <slot name="description">{{ description }}</slot>
-//       </div>
-//     </div>
-//   </div>
-// </template>
-//
-// <script>
-// export default {
-//   name: 'ElStep',
-//
-//   props: {
-//     title: String,
-//     icon: String,
-//     description: String,
-//     status: String
-//   },
-//
-//   data() {
-//     return {
-//       index: -1,
-//       lineStyle: {},
-//       internalStatus: ''
-//     };
-//   },
-//
-//   beforeCreate() {
-// //     this.$parent.steps.push(this);
-// const instance = getCurrentInstance()
-// let parent = instance?.parent
-//   },
-//
-//   beforeDestroy() {
-//     const steps = this.$parent.steps;
-//     const index = steps.indexOf(this);
-//     if (index >= 0) {
-//       steps.splice(index, 1);
-//     }
-//   },
-//
-//   computed: {
-//     currentStatus() {
-//       return this.status || this.internalStatus;
-//     },
-//     prevStatus() {
-//       const prevStep = this.$parent.steps[this.index - 1];
-//       return prevStep ? prevStep.currentStatus : 'wait';
-//     },
-//     isCenter() {
-//       return this.$parent.alignCenter;
-//     },
-//     isVertical() {
-//       return this.$parent.direction === 'vertical';
-//     },
-//     isSimple() {
-//       return this.$parent.simple;
-//     },
-//     isLast() {
-//       const parent = this.$parent;
-//       return parent.steps[parent.steps.length - 1] === this;
-//     },
-//     stepsCount() {
-//       return this.$parent.steps.length;
-//     },
-//     space() {
-//       const { isSimple, $parent: { space } } = this;
-//       return isSimple ? '' : space ;
-//     },
-//     style: function() {
-//       const style = {};
-//       const parent = this.$parent;
-//       const len = parent.steps.length;
-//
-//       const space = (typeof this.space === 'number'
-//         ? this.space + 'px'
-//         : this.space
-//           ? this.space
-//           : 100 / (len - (this.isCenter ? 0 : 1)) + '%');
-//       style.flexBasis = space;
-//       if (this.isVertical) return style;
-//       if (this.isLast) {
-//         style.maxWidth = 100 / this.stepsCount + '%';
-//       } else {
-//         style.marginRight = -this.$parent.stepOffset + 'px';
-//       }
-//
-//       return style;
-//     }
-//   },
-//
-//   methods: {
-//     updateStatus(val) {
-//       const prevChild = this.$parent.$children[this.index - 1];
-//
-//       if (val > this.index) {
-//         this.internalStatus = this.$parent.finishStatus;
-//       } else if (val === this.index && this.prevStatus !== 'error') {
-//         this.internalStatus = this.$parent.processStatus;
-//       } else {
-//         this.internalStatus = 'wait';
-//       }
-//
-//       if (prevChild) prevChild.calcProgress(this.internalStatus);
-//     },
-//
-//     calcProgress(status) {
-//       let step = 100;
-//       const style = {};
-//
-//       style.transitionDelay = 150 * this.index + 'ms';
-//       if (status === this.$parent.processStatus) {
-//         step = this.currentStatus !== 'error' ? 0 : 0;
-//       } else if (status === 'wait') {
-//         step = 0;
-//         style.transitionDelay = (-150 * this.index) + 'ms';
-//       }
-//
-//       style.borderWidth = step && !this.isSimple ? '1px' : 0;
-//       this.$parent.direction === 'vertical'
-//         ? style.height = step + '%'
-//         : style.width = step + '%';
-//
-//       this.lineStyle = style;
-//     }
-//   },
-//
-//   mounted() {
-//     const unwatch = this.$watch('index', val => {
-//       this.$watch('$parent.active', this.updateStatus, { immediate: true });
-//       this.$watch('$parent.processStatus', () => {
-//         const activeIndex = this.$parent.active;
-//         this.updateStatus(activeIndex);
-//       }, { immediate: true });
-//       unwatch();
-//     });
-//   }
-// };
-// </script>
-import { defineComponent, reactive, getCurrentInstance} from "vue";
+import { defineComponent, reactive, computed, getCurrentInstance, onBeforeUnmount, onMounted} from "vue";
 
 const ElStep = defineComponent({
   name: "ElStep",
@@ -194,17 +8,153 @@ const ElStep = defineComponent({
     description: String,
     status: String
   },
-  setup(props){
+  setup(props,{slots}){
     const instance = getCurrentInstance()
-    console.log(instance)
     let parent = instance?.parent
+    // @ts-ignore
     parent.steps.push(instance)
+    onBeforeUnmount (() => {
+      const steps = parent.steps;
+      const index = steps.indexOf(this);
+      if (index >= 0) {
+        steps.splice(index, 1);
+      }
+    })
+    onMounted(() =>{
+      const unwatch = instance.ctx.$watch('index', val => {
+        instance.ctx.$watch('parent.active', updateStatus, { immediate: true });
+        instance.ctx.$watch('parent.processStatus', () => {
+          const activeIndex = parent.active;
+          updateStatus(activeIndex);
+        }, { immediate: true });
+        unwatch();
+      });
+    })
     const state = reactive({
       index: -1,
       lineStyle: {},
       internalStatus: ''
+    });
+    // @ts-ignore
+    const currentStatus = computed(() => {
+      return props.status || state.internalStatus;
     })
-    return
+    const prevStatus = computed(() => {
+      const prevStep = parent.steps[state.index - 1];
+      return prevStep ? prevStep.currentStatus : 'wait';
+    })
+    const isCenter = computed(() =>{
+      return parent.alignCenter;
+    })
+    const isVertical = computed(()=> {
+      return parent.direction === 'vertical';
+    })
+    const isSimple = computed(() => {
+      return parent.simple;
+    })
+    const isLast = computed(() => {
+      return parent.steps[parent.steps.length - 1] === instance;
+    })
+    const stepsCount = computed(() => {
+      return parent.steps.length;
+    })
+    const space = computed(() => {
+      const { isSimple, parent: { space } } = instance;
+      return isSimple ? '' : space.value ;
+    })
+    const style = computed(() => {
+      const style = {};
+      const len = parent.steps.length;
+
+      const space = (typeof space.value === 'number'
+        ? space.value + 'px'
+        : space.value
+          ? space.value
+          : 100 / (len - (isCenter.value ? 0 : 1)) + '%');
+      style.flexBasis = space;
+      if (isVertical.value) return style;
+      if (isLast.value) {
+        style.maxWidth = 100 / stepsCount.value + '%';
+      } else {
+        style.marginRight = -parent.stepOffset + 'px';
+      }
+
+      return style;
+    })
+    const updateStatus = (val) => {
+      const prevChild = parent.children[state.index - 1];
+
+      if (val > state.index) {
+        state.internalStatus = parent.finishStatus;
+      } else if (val === state.index && prevStatus.value !== 'error') {
+        state.internalStatus = parent.processStatus;
+      } else {
+        state.internalStatus = 'wait';
+      }
+
+      if (prevChild) prevChild.calcProgress(state.internalStatus);
+    }
+
+    const calcProgress = (status) => {
+      let step = 100;
+      const style = {};
+
+      style.transitionDelay = 150 * state.index + 'ms';
+      if (status === parent.processStatus) {
+        step = currentStatus.value !== 'error' ? 0 : 0;
+      } else if (status === 'wait') {
+        step = 0;
+        style.transitionDelay = (-150 * state.index) + 'ms';
+      }
+
+      style.borderWidth = step && !isSimple.value ? '1px' : 0;
+      parent.direction === 'vertical'
+        ? style.height = step + '%'
+        : style.width = step + '%';
+
+      state.lineStyle = style;
+    }
+    return () => (
+      <div
+        style={style.value}
+        class={["el-step",
+          !isSimple.value && `is-${parent.direction}`,
+          isSimple.value && 'is-simple',
+          isLast.value && !space.value && !isCenter.value && 'is-flex',
+          isCenter.value && !isVertical.value && !isSimple.value && 'is-center'
+         ]}>
+        <div class={[`is-${currentStatus.value}`, "el-step__head"]}>
+          <div
+            class="el-step__line"
+            style={isLast.value ? '' : { marginRight: parent.stepOffset + 'px' }}>
+            <i class="el-step__line-inner" style={state.lineStyle}></i>
+          </div>
+
+          <div class={`is-${props.icon ? 'icon' : 'text'}`}>
+          {
+            currentStatus.value !== 'success' && currentStatus.value !== 'error' ? <slot name="icon">
+              {props.icon ? <i class={["el-step__icon-inner", props.icon]}></i> : ""}
+                {!props.icon && !isSimple.value ? <div class="el-step__icon-inner">{ state.index +1 }</div> : ""}
+            </slot> : <i class={[{ 'el-icon-' + (currentStatus.value === 'success' ? 'check' : 'close')},
+              "el-step__icon-inner is-status"]}></i>
+          }
+          </div>
+         </div>
+        {/*// <!-- title & description -->*/}
+        <div class="el-step__main">
+          <div
+            ref="title"
+            class={['is-' + currentStatus.value,"el-step__title"]}>
+            {slots.title?.()}
+          </div>
+          {isSimple.value ? <div class="el-step__arrow"></div>
+           : <div
+            class={['is-' + currentStatus.value,"el-step__description"]}>
+          {slots.description?.()}
+          </div>}
+        </div>
+      </div>
+    )
   }
 })
 
